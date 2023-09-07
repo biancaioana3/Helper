93e5230d14c8ee7568c404fa7363066e57ea6303
BiancaIoana03 <95350637+BiancaIoana03@users.noreply.github.com>
2023-09-07 10:21:20 +0100

    laravel

---
+++ Laravel_10/install.php
@@ -0,0 +1,15 @@
+composer create-project laravel/laravel example-app
+
+php artisan serve
+
+//db
+php artisan make:migration create_students_table
+
+//after modified the up() method
+php artisan migare
+
+//controller
+php artisan make:controller StudentController --resource
+
+//model
+php artisan make:model Student
